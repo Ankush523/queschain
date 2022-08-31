8cefbd50ae1ed5a8742ea3369227b7efe57dbe78
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-31 21:15:28 +0530

    pages improved

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-black'>
+        <div className='flex flex-col justify-center p-[250px]'>
+            <label className='font-montserrat text-[50px] pb-[50px] text-white'>Asking Questions Improves Your Learning</label>
+            <button className='bg-white mx-[350px] p-[10px] text-[20px] rounded-md'>Let's Get Started</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
